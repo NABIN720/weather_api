85ec17d9853afdd53ab4f2235a893955b48b973b
NABIN720 <nabinbhattarai720@gmail.com>
2025-07-22 00:10:49 +0545

    uploaded weatherapp

---
+++ weatherapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import requests
+import datetime
+
+# Create your views here.
+
+def index(request):
+
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'kathmandu'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=meroapiho'
+    PARAMS = {'units':'metric'}
+
+    data = requests.get(url, PARAMS).json()
+    # print(data)
+
+    description = data['weather'][0]['description']
+    icon = data['weather'][0]['icon']
+    temp = data['main']['temp']
+
+    day = datetime.date.today()
+
+    return render(request,'weatherapp/index.html',{'description':description, 'icon':icon, 'temp':temp, 'day':day,'city':city})
